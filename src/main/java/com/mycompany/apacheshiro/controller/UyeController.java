d928e0d969d38e0e37979a03879974e29d62e6a9
Batuhan APAYDIN <bapaydin67@gmail.com>
2015-03-20 01:40:03 +0200

    düzeltilme yapıldı

--- src/main/java/com/mycompany/apacheshiro/controller/UyeController.java
+++ src/main/java/com/mycompany/apacheshiro/controller/UyeController.java
@@ -16,6 +16,7 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
+import org.primefaces.event.RowEditEvent;
 
 @ManagedBean(name = "uyeController")
 @ViewScoped
@@ -54,6 +55,61 @@ public class UyeController implements Serializable{
 
     }
 
+  public void uyeTablosunuGuncelle(RowEditEvent rowEditEvent) {
+        String uyeAdi = ((UyeBilgisi) rowEditEvent.getObject()).getUyeAdi();
+
+        FacesMessage facesMessage = new FacesMessage(uyeAdi + " adlı kişinin bilgilerini değiştirdiniz...");
+        FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+
+        String uyeYeniAdi = ((UyeBilgisi) rowEditEvent.getObject()).getUyeAdi();
+        String uyeYeniSoyadi = ((UyeBilgisi) rowEditEvent.getObject()).getUyeSoyadi();
+        String uyeYeniDogumTarihi = ((UyeBilgisi) rowEditEvent.getObject()).getDogumTarihi();
+        int uyeYeniTelNo = ((UyeBilgisi) rowEditEvent.getObject()).getTelefonNo();
+        String uyeYeniMailAdres = ((UyeBilgisi) rowEditEvent.getObject()).getEmail();
+        String uyeYeniCinsiyet = ((UyeBilgisi) rowEditEvent.getObject()).getCinsiyet();
+
+        UyeBilgisi yeniUyeBilgisi = (UyeBilgisi) rowEditEvent.getObject();
+        yeniUyeBilgisi.setUyeAdi(uyeYeniAdi);
+        yeniUyeBilgisi.setUyeSoyadi(uyeYeniSoyadi);
+        yeniUyeBilgisi.setDogumTarihi(uyeYeniDogumTarihi);
+        yeniUyeBilgisi.setTelefonNo(uyeYeniTelNo);
+        yeniUyeBilgisi.setEmail(uyeYeniMailAdres);
+        yeniUyeBilgisi.setCinsiyet(uyeYeniCinsiyet);
+
+        yeniUyeEkleService.uyeBilgisiGuncele(yeniUyeBilgisi);
+
+    }
+
+    public void uyeSil(RowEditEvent rowEditEvent) {
+        try {
+            int uyeId = ((UyeBilgisi) rowEditEvent.getObject()).getId();
+
+            secilenUye = (UyeBilgisi) rowEditEvent.getObject();
+            secilenUye.setId(uyeId);
+
+            boolean uyeSilindiMi = yeniUyeEkleService.uyeBilgisiSil(secilenUye);
+            if (uyeSilindiMi) {
+                MesajGoster = "Secilen Uye Başarıyla Silindi.";
+                FacesMessage facesMessage = new FacesMessage(MesajGoster, MesajGoster);
+                FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+                guncelUyeListesi = yeniUyeEkleService.uyeleriGetir();
+                uyeListesiVer = guncelUyeListesi;
+            } else {
+                MesajGoster = "Seçilen Uye Silinemedi.";
+                FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(MesajGoster, "Başarısız"));
+            }
+
+        } catch (Exception ex) {
+
+        }
+    }
+
+    public void onRowCancel(RowEditEvent event) {
+        FacesMessage facesMessage = new FacesMessage("Güncelleme İşlemi İptal Edildi...");
+        FacesContext.getCurrentInstance().addMessage(null, facesMessage);
+
+    }
+
     public UyeBilgisi getUyeBilgisi() {
         return uyeBilgisi;
     }
@@ -68,6 +124,14 @@ public class UyeController implements Serializable{
 
     public void setUyeListesiVer(List<UyeBilgisi> uyeListesiVer) {
         this.uyeListesiVer = uyeListesiVer;
+    }
+
+    public List<UyeBilgisi> getUyeListeVer_2() {
+        return uyeListeVer_2;
+    }
+
+    public void setUyeListeVer_2(List<UyeBilgisi> uyeListeVer_2) {
+        this.uyeListeVer_2 = uyeListeVer_2;
     }
 
 }
